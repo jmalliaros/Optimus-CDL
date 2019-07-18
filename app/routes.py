0d8ff696f42393d07fd4810eafa635e0e9afad51
Mathew Kallada <matkallada@gmail.com>
2019-07-18 18:13:39 -0400

    Add results

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app import app
 import app.optimusform as opform
 
 @app.route('/')
 def compute():
-    form = opform
-    return render_template('base.html', form=form)
+    return render_template('base.html')
+
+@app.route('/submit_program')
+def submit_program():
+    return jsonify({"results_block": render_template("results_block.html"), "status": "success"})
+
