7eebc1de86168dfb0d31faa784d15728d1c54461
Mathew Kallada <matkallada@gmail.com>
2019-07-18 16:19:38 -0400

    Add secret key

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from config import Config
 
 app = Flask(__name__)
+app.secret_key = 'dk9032ujr8923hf9eh23f3g279rfg12379gfg239uifh912uhf91uh9r12hfu1f29'
 app.port = int(os.environ.get('PORT', 6000))
 
 from app import routes
