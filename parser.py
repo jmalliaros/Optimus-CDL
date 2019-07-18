b4816aeab560bc1b8d57a835ac31352b373a5d28
Mathew Kallada <matkallada@gmail.com>
2019-07-18 11:05:45 -0400

    Manual compiler

--- parser.py
+++ parser.py
@@ -1,1 +1,22 @@
-def parse_optimization_model(user_string, type="ad-hoc"):
+def parse_optimization_model(user_string, compiler_type="manual"):
+	formulation = None
+	constraints = []
+
+	if compiler_type == "manual":
+		for t in user_string.split():
+			if t.startswith("minimize") or t.startswith("maximize"):
+				formulation = t.split(" ")[1:]
+				break
+		for t in user_string.split():
+			if t.startswith("subject to"):
+				constraints.append(t.split(" ")[2:])
+
+	return formulation, constraints
+
+
+if __name__ == "__main__":
+	d = """
+minimize 5*x + 8*x
+subject to x>=1
+	"""
+	parse_optimization_model(d.strip())
