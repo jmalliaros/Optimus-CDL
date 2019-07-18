81bd5f453ba020d21f40f6778a3eeda4e226d604
Mathew Kallada <matkallada@gmail.com>
2019-07-18 14:40:55 -0400

    Update converttoH

--- convertToH.py
+++ convertToH.py
@@ -1,14 +1,20 @@
+from optimus_parser import parse_optimization_model
 from sympy import *
 
-x,y = symbols('x y')
-obj = 2*x + y
-conlist = [[x+2*y,3],[x+y,1]]
+d = """
+min 5*x + 8*y
+subject to x + y == 1
+    """
+objective_function, constraints, variables = parse_optimization_model(d.strip())
+
+obj = objective_function
+conlist = constraints
 
 def problemToH(obj, conlist):
     nc = len(conlist)
     Hlist = []
     for i in range(0,nc):
-        Hlist.append((conlist[i][0]-conlist[i][1])**2)
+        Hlist.append((conlist[i][0]-conlist[i][2])**2)
 
     Hlist.append(obj)
     return sum(Hlist)
